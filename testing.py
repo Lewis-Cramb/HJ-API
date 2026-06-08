d416e8ce91fa945a784624583bd88f98ba60ebd5
Lewis Cramb <lewiscramb@icloud.com>
2026-06-08 14:53:08 +0100

    Fix/Test - SF posting

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
+#currently testing SF POSTing
 import requests as rqs
 import datetime as dt
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 workingDate = dt.datetime.now().strftime("%A")
 
@@ -14,32 +15,6 @@ def oldHeader(filename): #Use this function if you do not need the "bearer" in t
         token = rf.read() #Tokens for sales platforms
     return {"Authorization":f"{token}"}
 
-
-def getAPIs():
-
-    #Define the commerce platform header here (i.e VirtualStock, Mirakl, TrueCommerce)
-    vs_headers = headerGeneration("vsToken")
-    mkl_headers = oldHeader("mklToken")
-
-
-    #All calls will need parameters so you can create a dictionary and add them here
-    vs_params = {"status":"ORDER_ACK"}
-    mkl_params = {"start_date":dt.datetime.now()-timedelta(1), "end_date":dt.datetime.now()} #Get all orders from yesterday
-    if workingDate == "Monday": #Subtract 3 days from the start if its a monday due to weekend orders (i.e friday-sunday)
-        mkl_params["start_date":dt.datetime.now()] = timedelta(3)
-
-    #Call APIs using rqs.get() to get the data
-    vs_resp = rqs.get("https://api.sandbox.virtualstock.com/restapi/v4/orders/?format=json",headers=vs_headers, params=vs_params) #Get all acknowledged orders from VirtualStock
-    mkl_resp = rqs.get("https://your-instance.mirakl.net/api/orders",headers=mkl_headers, params=mkl_params)
-
-    #Finally, filter it and join it all together
-
-    #I need the customer name, customer address, order date, courier details including tracking number, and product - also need to send the name of the seller
-
-    #virtualstock - filter this by date too
-
-
-    return filtered_data
 
 def getSFProdId(instance_url, product, sf_headers):
     query = f"SELECT Id, Pricebook2Id FROM PricebookEntry WHERE Product2.Name = '{product}' AND IsActive = true"
@@ -124,3 +99,6 @@ def postAPI(orders):
             print(prod_response.json())
             print(f"{key} added")
 
+orders = [{"accName": "John Lewis D2C", "custName": "Lewis Cramb", "orderDate":"08/06/2026", "custEmail":"test@test.test", "custPhone":"0000000000", "shipName":"DX", "expDate":"08/06/2026", "products":{"Buffalo 5 Shelf Metal Cabinet":1},"cost":"450", "custPO":"000000000011111111111"}]
+
+postAPI(orders)
