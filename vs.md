16a1bb74b49309376c7722a9c2cd9959451551a1
Lewis Cramb <lewiscramb@icloud.com>
2026-06-10 22:30:25 +0100

    Feat - Started POST for first order

--- vs.md
+++ vs.md
@@ -24,8 +24,16 @@ To find - what is ORDER_URl
 
 as usual, username and password stored in appropriate txt files in txts/
 
+Maybe ORDER_URl is literally the url of the order when you open it in vs? will test rn
+seems likely given what variables are held where (i.e part_number, line_ref and quantity are all stored in response.json()[results][items] instead of just [results])
+
+This hasn't worked, it's getting late so I'm going to leave it for now and come back on 22nd - reckon this is a test or think I'm able to email back to ask questions?
+
+
 # commit history
 
 a72dd99 - Setup the files
 
-[current commit] - connected to the vs server using credentials
+070dd68 - connected to the vs server using credentials
+
+[current commit] - attempting the POST for acknowledging an order (using the first order 230003426)
--- vs.py
+++ vs.py
@@ -1,17 +1,32 @@
 #this file will be for testing the VS sandbox
 import requests as rqs
 import base64
+import json as js
 
+#auth stuff
 username = open("txts/vsUsername.txt","r").read().strip()
 password = open("txts/vsPassword.txt","r").read().strip()
 
-# Create Basic Auth header
 credentials = base64.b64encode(f"{username}:{password}".encode()).decode()
 header = {
     "Authorization": f"Basic {credentials}"
 }
+
+#get all orders (may not be used)
 response = rqs.get("https://api.sandbox.virtualstock.com/restapi/v4/orders/?format=json",headers=header,params={"status":"ORDER"})
+json = response.json()
+y87 = json["results"][0]
+#post an acknowledgement
 
-print(response)
-print(response.json())
+header['Content-Type'] = "application/json"
+items = []
+for item in y87["items"]:
+    temp = {}
+    temp["part_number"] = item["part_number"]
+    temp["line_ref"] = item["line_reference"]
+    temp["quantity"] = item["quantity"]
+    items.append(temp)
 
+info = js.dumps(items)
+
+ackY87 = rqs.post("https://api.sandbox.virtualstock.com/restapi/v4/orders/287fe779-a5fb-4434-8903-4b421510056d/acknowledge/?format=json",headers=header,data=info)
