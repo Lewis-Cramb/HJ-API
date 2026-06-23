3a8fd3c4d4f56ba20aa5c7fb37ec07a38716865e
Lewis Cramb <lewiscramb@icloud.com>
2026-06-23 10:44:09 +0100

    Feat - VirtualStock connection

--- GETvirtual.py
+++ GETvirtual.py
@@ -1,17 +1,17 @@
 #This file is going to be for reading all of the data
 import requests as rqs
 from datetime import date as dt
-from functions import headerGeneration, startDate
+from functions import vsHeader, startDate
 from GETerrors import handle
 
 
 def getVS():
     #Create the additional information for the request
-    vs_headers = headerGeneration("vsToken")
+    vs_headers = vsHeader()
     vs_params = {"status":"ORDER_ACK"}
 
     #Call APIs using rqs.get() to get the data
-    vs_resp = rqs.get("https://api.sandbox.virtualstock.com/restapi/v4/orders/?format=json",headers=vs_headers, params=vs_params) #Get all acknowledged orders from VirtualStock
+    vs_resp = rqs.get("https://api.virtualstock.com/restapi/v4/orders",headers=vs_headers, params=vs_params) #Get all acknowledged orders from VirtualStock
     error_resp = handle(vs_resp) #no need to do anything on success, can't refresh login as its not a token
     if error_resp == "Failure":
         return []
@@ -27,15 +27,18 @@ def getVS():
     for order in vs_data["results"]: #add phone number
         if dt.fromisoformat(order["order_date"][0:order["order_date"].index("T")]) < startDate(): 
             curr = {}
+            price = 0.0
             curr["accName"] = "John Lewis D2C"
             curr["custName"] = order["shipping_address"]["full_name"]
             curr["orderDate"] = order["order_date"][0:order["order_date"].index("T")]
             curr["custEmail"] = order["shipping_address"]["email"]
             curr["custPhone"] = order["shipping_address"]["phone"]
             curr["shipName"] = "TO GET"
-            curr["expDate"] = order["items"]["promised_date"][:order["items"]["promised_date"].index("T")]
-            curr["products"] = {}
-            curr["cost"] = order["total"]
+            curr["expDate"] = order["items"][0]["promised_date"][:order["items"][0]["promised_date"].index("T")]
+            curr["products"] = {}#
+            for item in order["items"]:
+                price += float(item["subtotal"])
+            curr["cost"] = f"{price}"
             curr["custPO"] = order["end_user_purchase_order_reference"]
 
             for product in curr["items"]:
@@ -45,4 +48,3 @@ def getVS():
 
         
     return filtered_orders
-
