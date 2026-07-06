011020101b9d773848fb4af025ed84a00979d2e3
Lewis Cramb <lewiscramb@icloud.com>
2026-07-06 10:19:29 +0100

    Feat - attempting labels

--- DX-Sandbox/dx.md
+++ DX-Sandbox/dx.md
@@ -6,4 +6,6 @@ I need to provide "a label or two" using the sandbox for verification and then I
 sandbox endpoint: https://itd.dx-track.com/DespatchManager.API.Service.DM6Lite_Test/DM6LiteService.svc
 
 
-I need to get authentication credentials
+I need to get authentication credentials - got them now
+
+was in the middle of doing the labels and realised that I need to get the consignment numbers first so will commit the attempt and then move onto the GET first
--- DX-Sandbox/dx.py
+++ DX-Sandbox/dx.py
@@ -17,3 +17,34 @@ namespace = {"ns": "http://schemas.datacontract.org/2004/07/DespatchManager.API.
 session_key = root.findtext("ns:SessionKey",namespaces=namespace)
 
 
+authHead = f"<AuthHeader><SessionKey>{session_key}</SessionKey></AuthHeader>"
+
+headers = {
+    "AuthHeader": authHead,
+    "Context-type": "text/xml charset=utf-8"
+}
+
+consignmentNumbers = ["L3014499","L3049429"]
+
+for number in consignmentNumbers:
+    payload = {
+        "ConsignmentNumber": number,
+        "LabelReturnType": 0,  # 0=PDF, 1=ZPL, 2=Datamax
+        "PrintSelection": 0,   # 0=unprinted labels
+        "RoutingStream": "F",
+        "PDFLabelConfig": {
+            "labelSetup": 1,      # 1 label per page
+            "startingPosition": 1
+        }
+    }
+
+    labelResponse = rqs.post("https://itd.dx-track.com/DespatchManager.API.Service.DM6Lite_Test/DM6LiteService.svc/GetLabels", json=payload, headers=headers)
+
+    print(labelResponse.text)
+    
+    label_data = ET.fromstring(labelResponse.text).findtext("ns:label",namespaces=namespace)
+
+    import base64
+    pdf_bytes = base64.b64decode(label_data)
+    with open(f"label-{number}.pdf", "wb") as f:
+        f.write(pdf_bytes)
